0f5d4a4760910c850adb4ebb6cea91ded5ae79f0
marcosarango <sar21005@byui.edu>
2021-11-20 16:36:54 -0500

    update

--- weather/js/date.js
+++ weather/js/date.js
@@ -29,6 +29,6 @@ function updateBox(){
     
     document.getElementById('full-date').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()]+ ' '+ date;}
 
-if (d.getDay() == 4){
+if (d.getDay() == 5){
     document.querySelector("#pancake").style.display = "block";
 }
