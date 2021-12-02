3a04675d43c2147d0362d9762e271d72852673b9
marcosarango <sar21005@byui.edu>
2021-12-01 21:49:25 -0500

    update

---
+++ lesson5/js/towncards.js
@@ -0,0 +1,47 @@
+fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (jsonObject) {
+    // console.table(jsonObject);   temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    const townname = towns.filter((town) => town.name === 'Preston' || town.name === 'Soda Springs' || town.name === 'Fish Haven');
+    townname.forEach((town) => {
+      let section = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let motto = document.createElement('p');
+      let yrfounded = document.createElement('p');
+      let population = document.createElement('p');
+      let annualrainfall = document.createElement('p');
+      let image = document.createElement('img');
+      let div = document.createElement('div');
+
+      h2.textContent = `${town.name}`;
+      motto.textContent = `${town.motto}`;
+      yrfounded.textContent = `Year Founded: ${town.yearFounded}`;
+      population.textContent = `Population: ${town.currentPopulation}`;
+      annualrainfall.textContent = `Annual Rain Fall: ${town.averageRainfall}`;
+
+      if (town.name === 'Preston') {
+        image.setAttribute('src', "/lesson5/images/florian.jpeg");
+        image.setAttribute('alt', "Hot air balloons over a valley with a town in the background.");
+      } else if (town.name === 'Soda Springs') {
+        image.setAttribute('src', "/lesson5/images/sajad.jpeg");
+        image.setAttribute('alt', "A store with a soda fountain sign and several soda refrigerators.");
+      } else {
+        image.setAttribute('src', "/lesson5/images/zoltan.jpeg");
+        image.setAttribute('alt', "A man flyfishing while wading in a river.");
+      }
+
+      div.appendChild(h2);
+      div.appendChild(motto);
+      div.appendChild(yrfounded);
+      div.appendChild(population);
+      div.appendChild(annualrainfall);
+      section.appendChild(div);
+      section.appendChild(image);
+
+      document.querySelector('div.sections').appendChild(section);
+    });
+  });
