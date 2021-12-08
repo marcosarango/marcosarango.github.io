9711108fe725ba09ed0a8b228402a80403c02df2
marcosarango <sar21005@byui.edu>
2021-12-08 14:20:40 -0500

    update

---
+++ lesson6/js/eventFish.js
@@ -0,0 +1,20 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];  // temporary checking for valid response and data parsing
+
+
+    for(let event of towns[2].events) {
+      let list =  document.createElement('li');
+      list.textContent = event;
+    
+
+    document.querySelector('.events').appendChild(list);
+    }
+  });
+  
